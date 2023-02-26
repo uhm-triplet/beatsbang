11299187c7b036a435954bf5906fb94617ddffb9
yongu2000 <yongu200@hawaii.edu>
2023-02-25 17:17:50 -1000

    Update beatmap
    about 30 sec

--- Assets/Rhythm Part/Scripts/Note.cs
+++ Assets/Rhythm Part/Scripts/Note.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    public enum Type
+    {
+        Left,
+        Right
+    }
     Rigidbody2D rb;
+    [HideInInspector] public Type type;
     public float speed;
     [HideInInspector] public Vector2 SpawnPos;
     [HideInInspector] public Vector2 HitPos;
     [HideInInspector] public Vector2 RemovePos;
     [HideInInspector] public float TimeBeforeHit;
     [HideInInspector] public float SpawnTime;
-    Activator activator;
+    Activator activatorL;
+    Activator activatorR;
 
 
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        activator = GameObject.Find("Activators/Activator1").GetComponent<Activator>();
+        activatorL = GameObject.Find("Activators/Activator1").GetComponent<Activator>();
+        activatorR = GameObject.Find("Activators/Activator2").GetComponent<Activator>();
     }
 
     // Start is called before the first frame update
@@ -34,7 +42,10 @@ public class Note : MonoBehaviour
 
         if (transform.position == new Vector3(RemovePos.x, RemovePos.y, 0))
         {
-            activator.OnFail();
+            if (type == Type.Left)
+                activatorL.OnFail();
+            else if (type == Type.Right)
+                activatorR.OnFail();
             Destroy(gameObject);
         }
     }
--- Assets/Rhythm Part/Scripts/SongManager.cs
+++ Assets/Rhythm Part/Scripts/SongManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SongManager : MonoBehaviour
 {
-
     //the current position of the song (in seconds)
     public float songPosition;
 
@@ -12,11 +11,57 @@ public class SongManager : MonoBehaviour
     float dsptimesong;
 
     // Decides the speed of the note. Higher this value, slower the note.
-    float timeBeforeHit = 2f;
+    float timeBeforeHit = 2.5f;
 
     //keep all the position-in-time of notes in the song
-    float[] notesLeft = { 2.55f, 3.09f, 4.09f, 4.54f, 5.11f, 5.67f, 6.18f, 6.71f, 7.55f, 8.09f, 9.09f, 9.54f, 10.11f, 10.67f, 11.18f, 11.71f };
-    float[] notesRight = { 2.55f, 3.09f, 4.09f, 4.54f, 5.11f, 5.67f, 6.18f, 6.71f, 7.55f, 8.09f, 9.09f, 9.54f, 10.11f, 10.67f, 11.18f, 11.71f };
+    float[] notesLeft = { 2.85f, 3.05f, 3.25f, 3.45f, 3.65f, //basic pattern: 0.2f, next pattern 0.9f
+                         4.55f, 4.75f, 4.95f, 5.15f, 5.35f,
+                         6.25f, 6.45f, 6.65f, 6.85f, 7.05f,
+                        7.95f, 8.15f, 8.35f, 8.55f, 8.75f,
+                        9.65f, 9.85f, 10.05f, 10.25f, 10.45f,
+                        11.35f, 11.55f, 11.75f, 11.95f, 12.15f,
+                        13.05f, 13.25f, 13.45f, 13.65f, 13.85f,
+                            14.85f, 15.05f, 15.25f, 15.45f,
+                            16.55f, 16.75f, 16.95f, 17.15f,
+                            18.25f, 18.45f, 18.65f, 18.85f,
+                            19.95f, 20.15f, 20.35f, 20.55f,
+                        // left right switch
+                        21.55f, 21.75f, 21.95f, 22.75f, 23.35f, 23.75f, 24.55f,
+                        25.05f, 25.65f,
+                        26.75f, 27.15f, 27.55f,
+
+                        28.35f, 28.80f, 29.65f, 30.05f, 30.55f, 30.75f, 31.35f,
+
+                        //연타
+                        31.95f, 32.35f, 32.55f,
+
+                        33.70f, 34.10f, 34.30f,
+                          };
+    float[] notesRight = { 4.05f, 5.75f, 7.55f, // pattern 1: 
+                            7.95f, 8.55f, 9.15f,
+                            9.65f, 10.25f, 10.85f,
+                            11.35f, 11.95f, 12.55f,
+                            13.05f, 13.65f, 14.25f,
+                            14.75f, 14.95f, 15.15f, 15.75f, 15.95f, 16.45f, 16.85f, 17.65f,
+                            18.45f, 18.85f, 19.45f,
+                            20.35f, 20.55f, 21.15f,
+                            
+                            // left right switch
+                            23.45f, 23.65f, 23.85f, 24.05f, 24.25f,
+                            25.15f, 25.35f, 25.55f, 25.75f, 25.95f,
+                            26.85f, 27.05f, 27.25f, 27.45f, 27.65f,
+                            28.55f, 28.75f, 29.15f, 29.35f,
+                            30.25f, 30.45f, 30.85f, 31.05f,
+
+                            //연타
+                            31.75f, 32.25f, 32.45f, 33.00f,
+
+                            33.50f, 34.00f, 34.20f,
+
+
+
+
+    };
 
     //the index of the next note to be spawned
     int nextIndexLeft = 0;
@@ -53,6 +98,7 @@ public class SongManager : MonoBehaviour
             n.RemovePos = removePosLeft;
             n.TimeBeforeHit = timeBeforeHit;
             n.SpawnTime = songPosition;
+            n.type = Note.Type.Left;
 
             nextIndexLeft++;
         }
@@ -67,7 +113,7 @@ public class SongManager : MonoBehaviour
             n.RemovePos = removePosRight;
             n.TimeBeforeHit = timeBeforeHit;
             n.SpawnTime = songPosition;
-
+            n.type = Note.Type.Right;
             nextIndexRight++;
         }
     }
